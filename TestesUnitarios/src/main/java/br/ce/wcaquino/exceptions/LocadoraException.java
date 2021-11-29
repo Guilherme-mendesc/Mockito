904cd9c9f00fd1a68c7ce05bdba5892d11598be0
Guilherme-mendesc <guimendes1337@gmail.com>
2021-11-29 15:27:01 -0300

    Mockito

---
+++ TestesUnitarios/src/main/java/br/ce/wcaquino/exceptions/LocadoraException.java
@@ -0,0 +1,10 @@
+package br.ce.wcaquino.exceptions;
+
+public class LocadoraException extends Exception {
+
+	public LocadoraException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+}
